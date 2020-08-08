9ad06c9e7cd49dd738f75c64a996529a2038ca5e
okj203 <okj203@gmail.com>
2020-08-08 17:29:48 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
+const lessons = require("./routes/api/lessons");
+const schedules = require("./routes/api/schedules");
+const reviews = require("./routes/api/reviews");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch((err) => console.log(err));
+
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => res.send("CAMP")); // request response- gets printed
+
+app.use(passport.initialize());
+require("./config/passport")(passport);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); // parses data into json and passes it to the routes (order matters!)
+
+app.use("/api/users", users);
+app.use("/api/lessons", lessons);
+app.use("/api/schedules", schedules);
+app.use("/api/reviews", reviews);
---
+++ models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    accountType: {
+      type: String,
+      required: true,
+    },
+    // firstName: {
+    //   type: String,
+    //   required: true,
+    // },
+    // lastName: {
+    //   type: String,
+    //   required: true,
+    // },
+    // birthDay: {
+    //   type: Date,
+    //   required: true,
+    // },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = User = mongoose.model("User", UserSchema);
