732ccc0c28850886d9481e5312456731e445269b
Paul Gonzalez <paulgonzalez21@live.com>
2020-08-10 20:54:57 -0400

    frontend

--- frontend/src/components/main/main_page.jsx
+++ frontend/src/components/main/main_page.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import Navbar from '../nav/navbar_form.jsx'
+import Navbar from '../nav/navbar_form.jsx';
 import splash_img from '../../images/splash_image.jpg';
+import './main.css';
 
 class MainPage extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className='test'>
         <Navbar className='navbar' />
         <div className='splash-container'>
           <img src={splash_img}/>
