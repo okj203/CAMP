3c4656a46129d88b0663c8ac03be56871c3f9d03
Mac9330 <macyoung94@gmail.com>
2020-08-12 17:19:32 -0400

    fix auth

--- frontend/src/reducers/session_reducer.js
+++ frontend/src/reducers/session_reducer.js
@@ -1,6 +1,7 @@
 import {
   RECEIVE_CURRENT_USER,
   RECEIVE_USER_LOGOUT,
+  RECEIVE_USER_SIGN_IN
 } from '../actions/session_actions';
 
 const _nullSession = {
@@ -12,6 +13,7 @@ export default (state = _nullSession, action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
+      debugger
       return {
         ...state,
         isAuthenticated: !!action.currentUser,
@@ -19,6 +21,11 @@ export default (state = _nullSession, action) => {
       };
     case RECEIVE_USER_LOGOUT:
       return _nullSession;
+    case RECEIVE_USER_SIGN_IN:
+      return {
+        ...state,
+        isSignedIn: true
+      };
     default:
       return state;
   }
