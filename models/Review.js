b239f3c209b591cbd56bde8945a435be0dec2f2d
[Anthony Cruz] <[anthonyecruz@gmail.com]>
2020-08-12 21:18:02 -0400

    post reviews functional

--- models/Review.js
+++ models/Review.js
@@ -1,23 +1,28 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const ReviewSchema = new Schema({
-  rating: {
-    type: String,
-    required: true,
+const ReviewSchema = new Schema(
+  {
+    rating: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    reviewer_id: {
+      type: Schema.Types.ObjectId,
+      ref: "users",
+    },
+    teacher_id: {
+      type: Schema.Types.ObjectId,
+      ref: "users",
+    },
   },
-  description: {
-    type: String,
-    required: true,
-  },
-  reviewer_id: {
-    type: Schema.Types.ObjectId,
-    ref: "users",
-  },
-  teacher_id: {
-    type: Schema.Types.ObjectId,
-    ref: "users",
-  },
-});
+    {
+  timestamps: true,
+    }
+);
 
 module.exports = Tweet = mongoose.model("Review", ReviewSchema);
