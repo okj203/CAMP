6186317f0b6baec6b50a58c8b130f69980aef448
Mac9330 <macyoung94@gmail.com>
2020-08-14 00:03:58 -0400

    update teacher dash

--- frontend/src/components/dashboard/teacher_dashboard.jsx
+++ frontend/src/components/dashboard/teacher_dashboard.jsx
@@ -3,17 +3,24 @@ import React from "react"
 import EventCalendar from "../calendar/calendar";
 
 const TeacherDash = ({ user, events, defaultView, teacherId}) => {
+    
     if (user[teacherId]) {
         const teacher = user[teacherId];
         return(
             <div>
+                
                 {/* <h1>{teacher.fname} {teacher.lname}'s Dashboard</h1> */}
                 <h2>{teacher.fname} {teacher.lname}'s Scheduled Classes</h2>
                 <EventCalendar events={events} defaultView={defaultView}  />
             </div>
         )
     } else {
-        return null
+        return(
+            <div>
+                <h1>{user.fname} {user.lname}'s Dashboard</h1>
+                <EventCalendar user={user} events={events} defaultView={defaultView} />
+            </div>
+        )
     }
 }
 
