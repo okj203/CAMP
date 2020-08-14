dd242de3e4f1e29b5cd8af58edd83342753d86e0
Paul Gonzalez <paulgonzalez21@live.com>
2020-08-13 22:34:18 -0400

    fix clear errors

--- frontend/src/components/session/signup_container.js
+++ frontend/src/components/session/signup_container.js
@@ -1,9 +1,5 @@
 import { connect } from 'react-redux';
-<<<<<<< HEAD
-import { signup, login } from '../../actions/session_actions';
-=======
 import {signup, login, clearErrors } from '../../actions/session_actions';
->>>>>>> 79066c88cce4526700abc5f19563535b9d8e0343
 import SessionForm from './session_form';
 
 const mstp = state => ({
@@ -14,13 +10,9 @@ const mstp = state => ({
 
 const mdtp = dispatch => ({
   processForm: user => dispatch(signup(user)),
-<<<<<<< HEAD
-  login: user => dispatch(login(user))
-=======
   login: user => dispatch(login(user)),
   clearErrors: () => dispatch(clearErrors())
 
->>>>>>> 79066c88cce4526700abc5f19563535b9d8e0343
 });
 
 export default connect(mstp, mdtp)(SessionForm);
