e8895acd912ba80885c9232629163aae187f253c
[Anthony Cruz] <[anthonyecruz@gmail.com]>
2020-08-13 20:35:57 -0400

    teacher reviews cleaned up

--- frontend/src/components/user/teacher_reviews.jsx
+++ frontend/src/components/user/teacher_reviews.jsx
@@ -5,32 +5,6 @@ import './users.scss'
 
 class User extends React.Component {
 
-  // showHeader() {
-  //   const { teacher } = this.props;
-  //   return (
-  //     <div className="show-info-header">
-  //       <h1>{teacher.fname} {teacher.lname}</h1>
-  //       <p>{teacher.email}</p>
-  //     </div>
-  //   )
-  // }
-
-  mapReviews() {
-    const { reviews, teacher, teacherId } = this.props;
-    // if (teacher) {
-    // return (
-    //   reviews.map((review, idx) => (
-    //     console.log(teacher[review.reviewer_id]),
-    //     console.log(review.reviewer_id),
-    //     <li key={idx}>
-    //       <p>{review.rating} Stars</p>
-    //       <p>{review.description}</p>
-    //       <p>{teacher[review.reviewer_id]}</p>
-    //     </li>
-    //   ))
-    // );
-  }
-
   showReviews() {
     const { reviews, teacher, teacherId } = this.props;
     // const { fname, lname } = teacher ? teacher : null
@@ -43,14 +17,9 @@ class User extends React.Component {
       );
     } else 
       if (reviewsLength || Object.values(teacher).length < 2) {
-      // console.log(reviews.map(review => (
-      //   teacher[review.reviewer_id].accountType
-      // )))
       const id = reviews[0].reviewer_id
-      // console.log(teacher[id].username)
       return (
         <div className="reviews-index">
-          {/* <h1>{this.showHeader()}</h1> */}
           <h1>{teacherInfo.fname} {teacherInfo.lname}'s reviews!</h1>
           <div className="each-review">
             {reviews.map((review, idx) => (
@@ -61,7 +30,6 @@ class User extends React.Component {
                 <p>{teacher[review.reviewer_id].username}</p>
               </li>
             ))}
-            {this.mapReviews()}
           </div>
         </div>
       ); 
@@ -69,9 +37,8 @@ class User extends React.Component {
       return (
         <div>
           <div className="no-reviews">
-            <h2>There are no reviews</h2>
+            <h2>There are no reviews for this teacher</h2>
           </div>
-          {/* <h1>{teacherInfo.fname} {teacherInfo.lname}'s reviews!</h1> */}
         </div>
       );
     }
@@ -79,42 +46,6 @@ class User extends React.Component {
 
   render() {
     return this.showReviews();
-    // const { reviews, teacher, teacherId } = this.props;
-    // // const { fname, lname } = teacher ? teacher : null
-    // const reviewsLength = reviews.length
-    // const teacherInfo = teacher[teacherId]
-    // // console.log(teacherInfo)
-    // // console.log(teacher)
-    // if (typeof reviewsLength == "undefined" || reviews.length === 0) {
-    //   return (
-    //     <div>
-    //       <div className="no-reviews">
-    //         <h2>There are no reviews</h2>
-    //       </div>
-    //       {/* <h1>{teacherInfo.fname} {teacherInfo.lname}'s reviews!</h1> */}
-    //     </div>
-    //   );
-    // } else if (reviews) {
-    //   // console.log(reviews.map(review => (
-    //   //   teacher[review.reviewer_id].accountType
-    //   // )))
-    //   const id = reviews[0].reviewer_id
-    //   console.log(teacher[id].username)
-    //   return (
-    //     <div className="reviews-index">
-    //       {/* <h1>{this.showHeader()}</h1> */}
-    //       <h1>{teacherInfo.fname} {teacherInfo.lname}'s reviews!</h1>
-    //       <div className="each-review">
-    //         {reviews.map((review, idx) => (
-    //             <li key={idx}>
-    //               <p>{review.rating} Stars</p>
-    //               <p>{review.description}</p>
-    //             </li>
-    //         ))}
-    //       </div>
-    //     </div>
-    //   );
-    // }
   }
 }
 
